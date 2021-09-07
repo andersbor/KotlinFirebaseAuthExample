27fe1079805dc8aafd9c0aae6736c7ce41942b99
Anders Børjesson <anbo@zealand.dk>
2021-09-07 13:51:03 +0200

    Initial commit

---
+++ app/src/main/java/dk/easj/anbo/firebaseauthexample/FirstFragment.kt
@@ -0,0 +1,83 @@
+package dk.easj.anbo.firebaseauthexample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import dk.easj.anbo.firebaseauthexample.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        auth = Firebase.auth
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            binding.emailInputField.setText(currentUser.email) // half automatic login
+            // current user exists: No need to login again
+            // findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        binding.messageView.text = "Current user ${currentUser?.email}"
+        binding.buttonLogin.setOnClickListener {
+            val email = binding.emailInputField.text.toString().trim()
+            val password = binding.passwordInputField.text.toString().trim()
+            if (email.isEmpty()) {
+                binding.emailInputField.error = "No email"
+                return@setOnClickListener
+            }
+            if (password.isEmpty()) {
+                binding.passwordInputField.error = "No password"
+                return@setOnClickListener
+            }
+            // https://firebase.google.com/docs/auth/android/password-auth
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+                } else {
+                    binding.messageView.text = task.exception?.message
+                }
+            }
+        }
+        binding.buttonRegister.setOnClickListener {
+            val email = binding.emailInputField.text.toString().trim()
+            val password = binding.passwordInputField.text.toString().trim()
+            if (email.isEmpty()) {
+                binding.emailInputField.error = "No email"
+                return@setOnClickListener
+            }
+            if (password.isEmpty()) {
+                binding.passwordInputField.error = "No password"
+                return@setOnClickListener
+            }
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    binding.messageView.text = "User created. Now please login"
+                    // Alternative: goto next fragment (no need to login after register)
+                } else {
+                    binding.messageView.text = task.exception?.message
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/dk/easj/anbo/firebaseauthexample/SecondFragment.kt
@@ -0,0 +1,41 @@
+package dk.easj.anbo.firebaseauthexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import dk.easj.anbo.firebaseauthexample.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textviewSecond.text = "Welcome " + Firebase.auth.currentUser?.email
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
