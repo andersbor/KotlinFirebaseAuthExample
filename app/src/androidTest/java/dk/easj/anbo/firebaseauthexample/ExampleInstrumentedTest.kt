a95be088cc6a79e7ba582617cf26f30628dee0e4
Anders Børjesson <anbo@zealand.dk>
2021-09-23 10:19:38 +0200

    UI test (Espresso) included

--- app/src/androidTest/java/dk/easj/anbo/firebaseauthexample/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/dk/easj/anbo/firebaseauthexample/ExampleInstrumentedTest.kt
@@ -1,5 +1,10 @@
 package dk.easj.anbo.firebaseauthexample
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,18 +12,39 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("dk.easj.anbo.firebaseauthexample", appContext.packageName)
+
+        onView(withText("Authentication")).check(matches(isDisplayed()));
+        onView(withId(R.id.emailInputField))
+            .perform(clearText())
+            .perform(typeText("anbo@secret6.dk"));
+        onView(withId(R.id.passwordInputField))
+            .perform(clearText())
+            .perform(typeText("secret6"))
+            .perform(closeSoftKeyboard());
+        onView(withId(R.id.sign_in)).perform(click());
+        pause(1000); // to wait for Firebase response to arrive
+        onView(withId(R.id.textview_second)).check(matches(withText(("Welcome anbo@secret6.dk"))));
+    }
+
+    private fun pause(millis: Long) {
+        try {
+            Thread.sleep(millis)
+            // https://www.repeato.app/android-espresso-why-to-avoid-thread-sleep/
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
     }
 }
--- app/src/main/java/dk/easj/anbo/firebaseauthexample/MainActivity.kt
+++ app/src/main/java/dk/easj/anbo/firebaseauthexample/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import dk.easj.anbo.firebaseauthexample.databinding.ActivityMainBinding
@@ -39,6 +40,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+        /*if (FirebaseAuth.getInstance().currentUser == null) {
+            // TODO does not update after login: need observable property (ViewModel)
+            val menuItem = menu.findItem(R.id.action_signout)
+            menuItem.isVisible = false
+        }*/
         return true
     }
 
@@ -49,10 +55,14 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             R.id.action_signout -> {
-                Firebase.auth.signOut()
-                findNavController(R.id.nav_host_fragment_content_main).popBackStack()
-                // TODO go to home screen (FirstFragment), popUpTo
-                // https://developer.android.com/codelabs/android-navigation#6
+                if (Firebase.auth.currentUser != null) {
+                    Firebase.auth.signOut()
+                    val navController = findNavController(R.id.nav_host_fragment_content_main)
+                    navController.popBackStack(R.id.FirstFragment, false)
+                    // https://developer.android.com/codelabs/android-navigation#6
+                } else {
+                    Snackbar.make(binding.root, "Cannot sign out", Snackbar.LENGTH_LONG).show()
+                }
                 true
             }
             else -> super.onOptionsItemSelected(item)
